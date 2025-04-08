d38309936b1c98b232aa1a5a07659a5f9177c856
15010ait <15010@ait.nsw.edu.au>
2025-04-08 17:35:22 +1000

    main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     // Example for EBook
     EBook myEbook;
-    myEbook.setEBookDetails("Digital Learning", "Jane Doe", "123E", true, "2023-01-01", "PDF", 5.2);
+    myEbook.setEBookDetails("Digital Learning", "Jane Brown", "123E", true, "2023-01-01", "PDF", 5.2);
     myEbook.displayBookDetails();
     myEbook.downloadBook();
     myEbook.borrowBook();
@@ -15,7 +15,7 @@ int main() {
 
     // Example for PrintedBook
     PrintedBook myPrintedBook;
-    myPrintedBook.setPrintedBookDetails("C++ Mastery", "John Smith", "456P", true, "2022-10-10", 300, "Shelf A3");
+    myPrintedBook.setPrintedBookDetails("C++ Mastery", "Paul Smith", "456P", true, "2022-10-10", 300, "Shelf A3");
     myPrintedBook.displayBookDetails();
     myPrintedBook.reserveShelfSpace();
     myPrintedBook.borrowBook();
